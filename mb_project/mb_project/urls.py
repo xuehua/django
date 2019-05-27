1b72283f9d8a449033463d619c693276d712081a
xuehua <xuehua@gmail.com>
2019-05-27 09:31:08 -0700

    Added codes to deploy static files on heroku.

--- mb_project/mb_project/urls.py
+++ mb_project/mb_project/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include #new
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf import settings  #new
+from django.conf.urls.static import static #new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
