f9599b85954010264bd91d1c53fa53d8a656c695
xuehua <xuehua@gmail.com>
2019-06-03 21:03:20 -0700

    Added features to create and update blogs.

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView # new
 
 from .models import Post
 
@@ -11,3 +12,12 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
 
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
