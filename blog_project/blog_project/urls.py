8f447d4920be9045032bdf4d97bf6f54bb677853
xuehua <xuehua@gmail.com>
2019-07-06 14:43:07 -0700

    Added changes for forms and user accounts.

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include #new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')), #new
+    path('accounts/', include('accounts.urls')),
     path('', include('blog.urls')), #new
 ]
