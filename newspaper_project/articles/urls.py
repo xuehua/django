9ba2c010631fb87409e02a99b7d70a03fa387e8f
xuehua <xuehua@gmail.com>
2019-09-22 15:12:50 -0700

    Added more codes for article editing and deleting for newspaper_project.

---
+++ newspaper_project/articles/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ArticleListView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
+urlpatterns = [
+    path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('', ArticleListView.as_view(), name='article_list'),
+]
--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from .models import Article
 # Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'body')
+    template_name = 'article_edit.html'
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
