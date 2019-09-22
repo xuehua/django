c830d72f4aecf8a1171857e561f89b031b167fa5
xuehua <xuehua@gmail.com>
2019-09-22 16:37:02 -0700

    Add the feature of new article for newspaper_project.

--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from .models import Article
 # Create your views here.
 
@@ -14,10 +15,15 @@ class ArticleDetailView(DetailView):
 
 class ArticleUpdateView(UpdateView):
     model = Article
-    fields = ('title', 'body')
+    fields = ('title', 'body',)
     template_name = 'article_edit.html'
 
 class ArticleDeleteView(DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body', 'author',)
