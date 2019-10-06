d2ccd3bd675b7fdc8b2b7d257b87abaec2a4e7d6
xuehua <xuehua@gmail.com>
2019-10-05 22:20:21 -0700

    Updated newspaper_project to display comments in article_list.html.

--- newspaper_project/articles/models.py
+++ newspaper_project/articles/models.py
@@ -18,7 +18,7 @@ class Article(models.Model):
         return reverse('article_detail', args=[str(self.id)])
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments',)
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
