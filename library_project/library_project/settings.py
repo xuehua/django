d3e7eebfeed2e3586252e53f49223c441d75af92
xuehua <xuehua@gmail.com>
2019-10-08 08:34:06 -0700

    implemented rest api interfaces for library_project.

--- library_project/library_project/settings.py
+++ library_project/library_project/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'rest_framework',
+    
     #local
     'books.apps.BooksConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
