dee100934e94c098d49fa0a579f57361bc58bdbc
xuehua <xuehua@gmail.com>
2019-10-12 15:29:01 -0700

    Added IsAuthenticated permission in views for blogAPI

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Post
 from .serializers import PostSerializer
@@ -8,9 +8,11 @@ from .serializers import PostSerializer
 
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
