a9d372ab540078e48deeb44d1490014c65bc9358
xuehua <xuehua@gmail.com>
2019-10-12 17:34:31 -0700

    Added a permission to only allow post author to update for PostDetail and to only allow users to create their own posts in PostList.

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 
+from .permissions import IsAuthorOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 # Create your views here.
@@ -12,7 +14,16 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def post(self, request, *args, **kwargs):
+        if request.user.id == int(request.data['author']):
+            #only allow users create their own posts.
+            return super().post(request, *args, **kwargs)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     #permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
