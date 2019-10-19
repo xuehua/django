ba668d04dbee40ae6d9901dbc86caf2288ce4e74
xuehua <xuehua@gmail.com>
2019-10-18 21:23:20 -0700

    Added schema and docs for blogapi

--- blogapi/blog_project/urls.py
+++ blogapi/blog_project/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view # new
 
+schema_view = get_schema_view(title='Blog API')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
     path('api-auth/', include('rest_framework.urls')), #one line setting to add login and logout
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('docs/', include_docs_urls(title='Blog API')),
+    path('schema/', schema_view),
 ]
