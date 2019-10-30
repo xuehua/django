0799b7bae9f434988ebe433db622f48df09c0170
xuehua <xuehua@gmail.com>
2019-10-29 20:07:30 -0700

    Added test for users for books project.

--- books/users/tests.py
+++ books/users/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username = 'user1',
+            email = 'user1@email.com',
+            password = 'testpass123'
+        )
+
+        self.assertEqual(user.username, 'user1' )
+        self.assertEqual(user.email, 'user1@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username = 'adminuser',
+            email = 'adminuser@email.com',
+            password = 'testpass123'
+        )
+
+        self.assertEqual(admin_user.username, 'adminuser')
+        self.assertEqual(admin_user.email, 'adminuser@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+    
+
