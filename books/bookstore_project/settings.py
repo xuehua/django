c2a16edfab33a1ae65d96f41e6968a0ee1c69aac
xuehua <xuehua@gmail.com>
2019-10-29 19:58:09 -0700

    Added CustomUser for the books project.

--- books/bookstore_project/settings.py
+++ books/bookstore_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.CustomUser'
