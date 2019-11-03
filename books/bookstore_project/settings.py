83b568ed5c7f2c36ac8b0af92a7b65930a9163e3
xuehua <xuehua@gmail.com>
2019-11-02 22:19:46 -0700

    Added the STATICFILES_FINDERS setting.

--- books/bookstore_project/settings.py
+++ books/bookstore_project/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
