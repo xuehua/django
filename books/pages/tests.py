f0f7740f51db76a4b5f6a09f55b082ce6c29ef14
xuehua <xuehua@gmail.com>
2019-11-06 21:12:16 -0800

    Added some tests for AboutPageTests for bookstore project.

--- books/pages/tests.py
+++ books/pages/tests.py
@@ -11,7 +11,6 @@ class HomePageTests(SimpleTestCase):
     def test_homepage_status_code(self):
         self.assertEqual(self.response.status_code, 200)
 
-    
     def test_homepage_template(self):
         self.assertTemplateUsed(self.response, 'home.html')
     
@@ -24,3 +23,15 @@ class HomePageTests(SimpleTestCase):
     def test_homepage_url_resolve_homepageview(self):
         view = resolve('/')
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
