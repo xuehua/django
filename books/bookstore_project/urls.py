330166573ed77f16e43a4d6e8ca07bf01db5c2f6
Xuehua Chen <xuehua@gmail.com>
2019-11-22 20:10:36 -0800

    adde BookListView for bookstore project and added url for the book list for the bookstore project.

--- books/bookstore_project/urls.py
+++ books/bookstore_project/urls.py
@@ -18,8 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('accounts/', include('django.contrib.auth.urls')),
-    #path('accounts/', include('users.urls')),
     path('accounts/', include('allauth.urls')),
     path('', include('pages.urls')),
+    path('books',include('books.urls'))
 ]
