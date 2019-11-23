632c7d2a4477527f0e0761481345316d2bea167e
Xuehua Chen <xuehua@gmail.com>
2019-11-22 21:43:59 -0800

    utilize get_absolute_url in booklist.html template for bookstore project.

--- books/books/models.py
+++ books/books/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.urls import reverse
+import logging 
 
+logger = logging.getLogger(__name__)
 # Create your models here.
 class Book(models.Model):
     
@@ -9,3 +12,8 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        url = reverse('book_detail', args=[str(self.id)])
+        logger.error(f'{self.id}, {url}, \n')
+        return url
