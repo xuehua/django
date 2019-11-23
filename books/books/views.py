906067044fc6215ae9c3a21b9ca4c06ea5d3e6f5
Xuehua Chen <xuehua@gmail.com>
2019-11-22 21:08:47 -0800

    Added detailed book view for bookstore project.

--- books/books/views.py
+++ books/books/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Book
 # Create your views here.
 
 class BookListView(ListView):
     model = Book
     context_object_name = 'book_list'
-    template_name = "books/booklist.html"
+    template_name = 'books/booklist.html'
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+
