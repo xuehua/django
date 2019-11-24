d02b54756a041870b6aa93325271cc9409c6a048
Xuehua Chen <xuehua@gmail.com>
2019-11-23 22:21:21 -0800

    Added Reviews model for bookstore project.

--- books/books/models.py
+++ books/books/models.py
@@ -1,6 +1,7 @@
 
 import logging 
 import uuid
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
@@ -20,3 +21,18 @@ class Book(models.Model):
         url = reverse('book_detail', args=[str(self.id)])
         logger.error(f'{self.id}, {url}, \n')
         return url
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book, 
+        on_delete=models.CASCADE, 
+        related_name='reviews'
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete = models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review
