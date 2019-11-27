4f23bee4b8e8396324c5e379b4cb634274aa1a52
xuehua <xuehua@gmail.com>
2019-11-27 08:59:24 -0800

    Added Stripe test keys for bookstore project.

--- books/orders/views.py
+++ books/orders/views.py
@@ -1,5 +1,11 @@
+from django.conf import settings
 from django.views.generic import TemplateView
 
 # Create your views here.
 class OrdersPageView(TemplateView):
     template_name = 'orders/purchase.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['stripe_key'] = settings.STRIPE_TEST_PUBLISHABLE_KEY
+        return context
