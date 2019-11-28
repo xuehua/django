e3f66a80cc9c571a3b406756528bd24a8f63b11f
xuehua <xuehua@gmail.com>
2019-11-28 13:16:27 -0800

    Added charge page for bookstore project.

--- books/orders/views.py
+++ books/orders/views.py
@@ -1,5 +1,10 @@
+import stripe
+from django.contrib.auth.models import Permission
 from django.conf import settings
 from django.views.generic import TemplateView
+from django.shortcuts import render
+
+stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
 
 # Create your views here.
 class OrdersPageView(TemplateView):
@@ -9,3 +14,17 @@ class OrdersPageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['stripe_key'] = settings.STRIPE_TEST_PUBLISHABLE_KEY
         return context
+
+def charge(request):
+    permission = Permission.objects.get(codename = "special_status")
+    u = request.user
+
+    u.user_permissions.add(permission)
+    if request.method == 'POST':
+        charge = stripe.Charge.create(
+            amount = 3900,
+            currency = 'usd',
+            description = 'Purchase all books',
+            source = request.POST['stripeToken']
+        )
+    return render(request, 'orders/charge.html')
