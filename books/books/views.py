508c2063fad47957df0a3b82fa4a5bf999d4015e
xuehua <xuehua@gmail.com>
2019-11-28 13:16:55 -0800

    Added basic search for the bookstore project.

--- books/books/views.py
+++ books/books/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin
@@ -19,4 +20,18 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    queryset = Book.objects.filter(title__icontains='beginners')
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
+
+
 
